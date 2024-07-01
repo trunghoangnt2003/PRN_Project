51245003ace2641bc235046c8699afe57b0d6979
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-07-01 19:00:12 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using Prism.Commands;
+using PRN_Project.MVVM;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace PRN_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
